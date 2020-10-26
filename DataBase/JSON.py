d91b8ad703426a43cdcab2a199b24d373365071d
sacortesro <sacortesro@unal.edu.co>
2020-10-25 20:21:07 -0500

    ya puedo acceder al JSON a través de diccionarios en Python

---
+++ DataBase/JSON.py
@@ -0,0 +1,10 @@
+import urllib3,json
+
+http = urllib3.PoolManager()
+url="https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+json_url = http.request('GET', url)
+#print(json_url.status)
+data=json.loads(json_url.data)
+#data = json.loads(json_url.read())
+for x in data["drinks"]:
+    print(x.get("strCategory"))    

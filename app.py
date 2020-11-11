e7e5d047defcc70f2240aaf455c5283df45e4c82
josmramirezmar <josmramirezmar@unal.edu.co>
2020-11-11 16:14:01 -0500

    comentado sqlachemy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template, request, url_for, session, f
 from datetime import timedelta
 from loged.coctel import coctel
 from config.userInf import userInf
-import sqlalchemy
+#import sqlalchemy
 
 app=Flask(__name__)
 app.register_blueprint(coctel,url_prefix="/loged")

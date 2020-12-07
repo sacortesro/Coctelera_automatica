fc511b74954b4f71de39ee8bc3c3caa9cb280599
Jose Manuel Ramirez Martinez <josmramirezmar@unal.edu.co>
2020-12-06 19:17:29 -0500

    mqtt

--- profile/__init__.py
+++ profile/__init__.py
@@ -2,10 +2,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mqtt import Mqtt
 
 # Global variables
 db = SQLAlchemy()
 login_manager=LoginManager()
+mqtt = Mqtt()
 
 def create_app():
     """Initialize the core aplication."""
@@ -15,6 +17,15 @@ def create_app():
     #inicializar plugins
     db.init_app(app)
     login_manager.init_app(app)
+    
+    #config mqtt
+    app.config['MQTT_BROKER_URL'] = '192.168.0.19'
+    app.config['MQTT_BROKER_PORT'] = 1883
+    app.config['MQTT_USERNAME'] = ''
+    app.config['MQTT_PASSWORD'] = ''
+    app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+    mqtt = Mqtt(app)
+    mqtt.publish('server', 'Servidor Iniciado')
 
     with app.app_context():
         # Import parts of our flask_wtforms_tutorial
